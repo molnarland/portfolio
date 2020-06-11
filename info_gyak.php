ccb5e69bc1ca5b6ea5bd2bbd9ed245f94ae34e26
Roland Molnár <roland.molnar@dpc.hu>
2020-06-11 16:08:40 +0200

    add sum

---
+++ info_gyak.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>faszság</title>
+</head>
+<body>
+<form method="post">
+    <input type="text" name="first" readonly value="<?php echo rand(1, 10); ?>">
+    <input type="text" name="second" readonly value="<?= rand(1, 10) ?>">
+    <input type="text" name="result">
+
+    <?php
+
+
+    if (isset($_POST['result'])) {
+        $num3 = (int)$_POST['first'] + (int)$_POST['second'];
+
+
+        echo 'actual result is ' . $num3;
+        echo ' you typed ' . $_POST['result'];
+
+        if ($_POST['result'] == $num3) {
+            echo ' you are good';
+        } else {
+            echo ' you\'re gay';
+        }
+    } else {
+        echo 'you typed nothing';
+    }
+
+    /*
+        if (isset($_POST['chance'])) {
+            var_dump($_POST['chance']);
+        }
+
+        $a = 1;
+        $b = 'szöveg';
+        $c = false;
+        $d = 1.45673;
+
+        if ('1' == 1) {
+            //true
+        }
+
+        if ('1' === 1) {
+            //false
+        }
+
+        $e = [
+            [1, 'egy', true],
+            [2, 'kettő', false],
+            [3, 'három', true],
+        ];
+
+        echo '<select name="chance">';
+        for ($i = 0; $i < count($e); $i++) {
+            echo '<option value="' . $e[$i][0] . '">' . $e[$i][1] . '</option>';
+        }
+        echo '</select>';
+    */
+    ?>
+    <button type="submit">küld</button>
+</form>
+<p>ewewew</p>
+
+</body>
+</html>
---
+++ sum.php
@@ -0,0 +1,56 @@
+<?php
+
+$numbers = [1, 2, 3, 9];
+$i = 0;
+$sum = 0;
+
+while ($i < count($numbers))
+{
+    echo $numbers[$i] . ' ';
+    $sum = $sum + $numbers[$i];
+    $i++;
+}
+echo '<br>összeg: ' . $sum;
+
+
+$sum = 0;
+for ($i = 0; $i < count($numbers); $i++)
+{
+    $sum = $sum + $numbers[$i];
+}
+echo '<br>2. összeg: '. $sum;
+
+
+echo '<br>';
+$sum = 0;
+foreach ($numbers as $number)
+{
+    echo $number . ' ';
+    $sum += $number;
+}
+echo '3. összeg: ' . $sum;
+
+
+echo array_sum($numbers);
+
+?>
+<form method="get">
+    <br>
+    <select name="numbers[]" size="100" multiple>
+<?php
+for ($i = 1; $i < 101; $i++)
+{
+    echo '<option value="' . $i . '">' . $i . '</option>';
+}
+?>
+    </select>
+    <button name="submit-button" type="submit">klikkelj csöves</button>
+</form>
+
+<?php
+
+if (isset($_GET['submit-button'], $_GET['numbers']))
+{
+    $numbers = $_GET['numbers'];
+    echo array_sum($numbers);
+}
